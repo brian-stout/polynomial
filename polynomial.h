d35d46d0b426ac6b1b41520936a76c949ed80c7f
Brian Stout <brianjonstout@gmail.com>
2017-01-13 23:03:03 -0500

    comments!

--- polynomial.h
+++ polynomial.h
@@ -18,56 +18,75 @@ struct term
 
 typedef struct term polynomial;
 
+/**	term_create() intializes a struct term based on the given coefficient and exponent values.
+*		should be used to start any new polynomial equations or create any lose terms.
+*/
 struct term *
 term_create( int, int );
 
+/**	poly_destroy() goes through each struct term in a link and frees up memory
+*       malloc'd during each term_create().  Use for each polynomial created in the program.		
+*/
 void
 poly_destroy( polynomial * );
 
+/**	poly_add_term() moves to the end of a polynomial equation and adds a new term to the end.
+*       The first argument should be the address of the first term.
+*       The second address will usually be a term returned from term_create()
+*
+*       ex: poly_add_term(&front, term_create(5, 5));		
+*/
 void
-poly_add_term( polynomial **, polynomial * );
+poly_add_term( polynomial **, struct term * );
 
+/**	poly_print() takes the data from a polynomial and prints out the terms in order.
+*/
 void
 poly_print( const polynomial * );
 
+/**	poly_to_string() outputs a polynomial similar to poly_print() but returns a character pointer
+*       instead of printing to stdout so the user can utilize the string for other purposes.
+*
+*       the function utilizes a GNU specific function asprintf() similar to snprintf but handles
+*           memory.
+*
+*       The user is required to free the character pointer when finished with it to avoid
+*           memory leaks.
+*
+*       TODO: implement snprintf() and malloc() to improve portability.
+*/
 char *
 poly_to_string( const polynomial * );
 
+/**	poly_add() returns a polynomial equation which is the sum of two other polynomials.
+*/
 polynomial *
 poly_add( const polynomial *, const polynomial * );
 
+/**	poly_sub() does the same thing as poly_add() but with subtraction.
+*/
 polynomial *
 poly_sub( const polynomial *, const polynomial * );
 
+/**	poly_equal() determines if two polynomials are equivalent to each other and returns the
+*       the relative 1 ("true") or 0 ("false")
+*/
 int
 poly_equal( const polynomial *, const polynomial * );
 
+/**	poly_iterate() moves through each term in a polynomial and runs the function in the second
+*        argument on it.
+*/
 void
 poly_iterate( polynomial *, void (*)(struct term *) );
 
+/**	test_print() is a boring function used to test poly_iterate.
+*       All it does is print out the coefficient and exponent of a term.
+*/
 void
 test_print( struct term * );
 
 double
 poly_eval(const polynomial *, double );
-//Write in a simplifier
-
-//Flourishes and challenges
-//Support all solutions via complex type
-//Multiplication function
-//*poly_mult(const polynomial *a, const polynomial);
-//Exponential function
-//*poly_pow(const polynomial *a, unsigned int e);
-//Solution function
-// poly_real_roots(const polynomial *p, double *solutions, size_t sz);
-//Real solutions function
-// division function
-// *poly_div(const polynomial *a, const polynomial *b, polynomial **r);
-//Solution function
-// poly_roots(polynomial *p, complex *solutions, size_t sz);
-//TODO: Write a highest coefficient finder to the print functions to determine if
-//          The + should be used in the prints
-//TODO: Fix the discrepencies between using polynominal versus struct term
-//          struct term should be used when only one term is expected
 
 #endif
