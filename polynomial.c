144457b0f7df3d1d0355a08235faa89832c181d1
Brian Stout <brianjonstout@gmail.com>
2017-01-13 21:52:29 -0500

    Ran polynomial c through indent to fix the formatting.

--- polynomial.c
+++ polynomial.c
@@ -1,9 +1,12 @@
 #include "polynomial.h"
 
-struct term *term_create(int coeff, int exp)
+struct term *
+term_create(int coeff, int exp)
 {
     struct term *node = malloc(sizeof(*node));
-    if(node) {
+
+    if (node)
+    {
         node->coeff = coeff;
         node->exp = exp;
         node->next = NULL;
@@ -11,37 +14,48 @@ struct term *term_create(int coeff, int exp)
     return node;
 }
 
-void poly_destroy(polynomial *eqn)
+void
+poly_destroy(polynomial * eqn)
 {
-    while(eqn) {
+    while (eqn)
+    {
         struct term *tmp = eqn->next;
+
         free(eqn);
         eqn = tmp;
     }
 }
 
-void poly_add_term(polynomial **front, polynomial *newTerm)
+void
+poly_add_term(polynomial ** front, polynomial * newTerm)
 {
     struct term *cursor;
+
     cursor = *front;
 
-    while(cursor->next != NULL)
+    while (cursor->next != NULL)
     {
         cursor = cursor->next;
     }
-    cursor->next=newTerm;
+    cursor->next = newTerm;
 }
 
-void poly_print(const polynomial *eqn)
+void
+poly_print(const polynomial * eqn)
 {
-    if(!eqn) {
+    if (!eqn)
+    {
         return;
     }
-    if(eqn->coeff) {
+    if (eqn->coeff)
+    {
         printf("%c%d", eqn->coeff > 0 ? '+' : '\0', eqn->coeff);
-        if(eqn->exp > 1) {
+        if (eqn->exp > 1)
+        {
             printf("x^%d", eqn->exp);
-        } else if (eqn->exp == 1) {
+        }
+        else if (eqn->exp == 1)
+        {
             printf("x");
         }
         printf(" ");
@@ -50,57 +64,68 @@ void poly_print(const polynomial *eqn)
 }
 
 //TODO: Test this more thoroughly
-char *poly_to_string(const polynomial *p)
+char *
+poly_to_string(const polynomial * p)
 {
     char sign;
     char *r;
-    if(!p) {
+
+    if (!p)
+    {
         return r;
     }
 
     //First part
-    if(p->coeff > 0)
+    if (p->coeff > 0)
     {
         sign = '+';
-    }   else if (p->coeff < 0) {
+    }
+    else if (p->coeff < 0)
+    {
         sign = '-';
     }
 
     //Prints out the coefficient string to a second string
-    if(p->exp > 1) {
-        asprintf(&r, "%c%dx^%d", sign, p->coeff*-1, p->exp);
-    } else if (p->exp == 1) {
-        asprintf(&r, "%c%dx", sign, p->coeff*-1);
-    } else {
-        asprintf(&r, "%c%d", sign, p->coeff*-1);
+    if (p->exp > 1)
+    {
+        asprintf(&r, "%c%dx^%d", sign, p->coeff * -1, p->exp);
+    }
+    else if (p->exp == 1)
+    {
+        asprintf(&r, "%c%dx", sign, p->coeff * -1);
+    }
+    else
+    {
+        asprintf(&r, "%c%d", sign, p->coeff * -1);
     }
 
-    if(p->next != NULL)
+    if (p->next != NULL)
     {
         asprintf(&r, "%s%s", r, poly_to_string(p->next));
     }
-    return r; 
+    return r;
 }
 
-polynomial *poly_add(const polynomial *a, const polynomial *b)
+polynomial *
+poly_add(const polynomial * a, const polynomial * b)
 {
     polynomial *polySum = term_create(0, 0);
-    
-    while(a != NULL && b != NULL)
+
+    while (a != NULL && b != NULL)
     {
-        if(a == NULL)
+        if (a == NULL)
         {
             poly_add_term(&polySum, term_create(b->coeff, b->exp));
-            b = b->next;            
+            b = b->next;
         }
-        else if(b == NULL)
+        else if (b == NULL)
         {
             poly_add_term(&polySum, term_create(a->coeff, a->exp));
             a = a->next;
         }
-        else if(a->exp == b->exp)
+        else if (a->exp == b->exp)
         {
-            poly_add_term(&polySum, term_create(a->coeff+b->coeff, a->exp));
+            poly_add_term(&polySum, term_create(a->coeff + b->coeff, a->exp));
             a = a->next;
             b = b->next;
         }
@@ -119,31 +144,33 @@ polynomial *poly_add(const polynomial *a, const polynomial *b)
     //Removes the zero, could replace this with the polynomial simplifier later on
     //  or add a specific function for removing exp 0s
     polynomial *cursor = polySum;
+
     polySum = polySum->next;
     free(cursor);
 
     return polySum;
 }
 
-polynomial *poly_sub(const polynomial *a, const polynomial *b)
+polynomial *
+poly_sub(const polynomial * a, const polynomial * b)
 {
     polynomial *polySum = term_create(0, 0);
-    
-    while(a != NULL && b != NULL)
+
+    while (a != NULL && b != NULL)
     {
-        if(a == NULL)
+        if (a == NULL)
         {
             poly_add_term(&polySum, term_create(b->coeff, b->exp));
-            b = b->next;            
+            b = b->next;
         }
-        else if(b == NULL)
+        else if (b == NULL)
         {
             poly_add_term(&polySum, term_create(a->coeff, a->exp));
             a = a->next;
         }
-        else if(a->exp == b->exp)
+        else if (a->exp == b->exp)
         {
-            poly_add_term(&polySum, term_create(a->coeff-b->coeff, a->exp));
+            poly_add_term(&polySum, term_create(a->coeff - b->coeff, a->exp));
             a = a->next;
             b = b->next;
         }
@@ -162,26 +189,28 @@ polynomial *poly_sub(const polynomial *a, const polynomial *b)
     //Removes the zero, could replace this with the polynomial simplifier later on
     //  or add a specific function for removing exp 0s
     polynomial *cursor = polySum;
+
     polySum = polySum->next;
     free(cursor);
 
     return polySum;
 }
 
-int poly_equal(const polynomial *a, const polynomial *b)
+int
+poly_equal(const polynomial * a, const polynomial * b)
 {
     int r = 1;
 
-    while(a != NULL)
+    while (a != NULL)
     {
-        if(b == NULL)
+        if (b == NULL)
         {
             r = 0;
             break;
         }
         if (a->coeff != b->coeff || a->exp != b->exp)
         {
-            r= 0;
+            r = 0;
             break;
         }
         else
@@ -193,27 +222,31 @@ int poly_equal(const polynomial *a, const polynomial *b)
     return r;
 }
 
-void poly_iterate(polynomial *p, void (*transform)(struct term *))
+void
+poly_iterate(polynomial * p, void (*transform) (struct term *))
 {
     struct term *cursor = p;
-    while(cursor != NULL)
+
+    while (cursor != NULL)
     {
         transform(cursor);
         cursor = cursor->next;
     }
 }
 
-void test_print(struct term *a)
+void
+test_print(struct term *a)
 {
     printf("The coeff is %d and the exp is %d\n", a->coeff, a->exp);
 }
 
-double poly_eval(const polynomial *p, double x)
+double
+poly_eval(const polynomial * p, double x)
 {
     double total = 0;
     double result = 0;
 
-    while(p != NULL)
+    while (p != NULL)
     {
         result = pow(x, p->exp);
         result *= p->coeff;
